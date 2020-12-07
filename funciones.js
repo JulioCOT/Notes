c247a287f06d1fab779d1fe7c15eed7756eb59eb
JulioCOT <72828680+JulioCOT@users.noreply.github.com>
2020-12-07 16:51:39 -0600

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -16,6 +16,7 @@ function inicializar(){
     textoNota = document.getElementById('texto-nota');
     temaNota = document.getElementById('tema-nota');
     contenedor=document.querySelector('.right');
+    msj=document.querySelector('.msj');
     //querySelector clases, id, etiquetas h2 h1 p div
     //querySelectorAll más de 1 div
 
@@ -24,14 +25,36 @@ function inicializar(){
 }
 
 function addNota(){
-    //objeto
-    nuevaNota = {
-        titulo:tituloNota.value,
-        texto:textoNota.value,
-        tema:temaNota.value,
-    };
-    //Agregar una nueva nota al array "notas"
-    notas.push(nuevaNota);
+    if(tituloNota.value!="" && textoNota.value!=""){
+//objeto
+nuevaNota = {
+    titulo:tituloNota.value,
+    texto:textoNota.value,
+    tema:temaNota.value,
+};
+//Agregar una nueva nota al array "notas"
+notas.push(nuevaNota);
 
-    contenedor.innerHTML+="<div class='nota "+ nuevaNota.tema+"'><h2>"+nuevaNota.titulo+"</h2><p>"+nuevaNota.texto+"</p></div>"
+contenedor.innerHTML+="<div class='nota "+ nuevaNota.tema+"'><div class= 'eliminar' id="+(notas.length-1)+">x</div><h2>"+nuevaNota.titulo+"</h2><p>"+nuevaNota.texto+"</p></div>"
+
+
+    }else{
+        msj.classList.add('mostrar')
+        setTimeout(function(){
+            msj.classList.remove('mostrar')
+        },3000)
+    }
+
+    eliminar=document.querySelectorAll('.eliminar');
+for(let i=0; i<notas.length;i++){
+    eliminar[i].addEventListener('click', eliminarNota, true)
+  
+    }
+
+}
+
+function eliminarNota(){
+    console.log("CLICK", event.target.id)
+    notas.splice(event.target.id,1)
+    document.getElementById(event.target.id).parentElement.remove();
 }
